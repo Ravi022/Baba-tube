fbd94755dc4b875f07d70b0cd258381bcd6b53a8
Ravi022 <vermaravifzd022@gmail.com>
2024-09-09 22:04:08 +0530

    make utils

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
 export const DB_NAME = "youtube";
+export const MAX_LIMIT_OF_DATA = "16kb";
+export const STORE_STATIC_DATA = "public";
--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import { connectDB } from "./db/db.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed :", error);
+  });
 
 /*
 import express from "express";
